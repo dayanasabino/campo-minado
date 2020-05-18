f40b61517e4c13b1317faa964ada90b3e441134b
Dayana Sabino <sabino.dayana@gmail.com>
2020-05-18 13:57:50 -0300

    App do jogo campo minado em Flutter

---
+++ lib/models/campo.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/foundation.dart';
+import 'explosao_exception.dart';
+
+class Campo {
+  final int linha;
+  final int coluna;
+  final List<Campo> vizinhos = [];
+
+  // Atributos privados
+  bool _aberto = false;
+  bool _marcado = false;
+  bool _minado = false;
+  bool _explodido = false;
+
+  // Construtor
+  Campo({
+    @required this.linha,
+    @required this.coluna,
+  });
+
+  // Função de adicionar vizinhos
+  void adicionarVizinho(Campo vizinho) {
+    final deltaLinha =
+        (linha - vizinho.linha).abs(); // valor abs - absoluto sempre positivo
+    final deltaColuna = (coluna - vizinho.coluna).abs();
+
+    if (deltaLinha == 0 && deltaColuna == 0) {
+      return;
+    }
+
+    if (deltaLinha <= 1 && deltaColuna <= 1) {
+      vizinhos.add(vizinho);
+    }
+  }
+
+  void abrir() {
+    if (_aberto) {
+      return;
+    }
+
+    _aberto = true;
+
+    if (_minado) {
+      _explodido = true;
+      throw ExplosaoException();
+    }
+
+    if (vizinhancaSegura) {
+      vizinhos.forEach((v) => v.abrir()); // gera uma recursividade
+    }
+  }
+
+  void revelarBomba() {
+    if (_minado) {
+      _aberto = true;
+    }
+  }
+
+  void minar() {
+    _minado = true;
+  }
+
+  void alternarMarcacao() {
+    _marcado = !_marcado;
+  }
+
+  void reiniciar() {
+    _aberto = false;
+    _marcado = false;
+    _minado = false;
+    _explodido = false;
+  }
+
+  bool get minado {
+    return _minado;
+  }
+
+  bool get explodido {
+    return _explodido;
+  }
+
+  bool get aberto {
+    return _aberto;
+  }
+
+  bool get marcado {
+    return _marcado;
+  }
+
+  bool get resolvido {
+    bool minadoEMarcado = minado && marcado;
+    bool seguroEAberto = !minado && aberto;
+
+    return minadoEMarcado || seguroEAberto;
+  }
+
+  // Cria um getter que retorna um boolean para saber se vizinhança é segura
+  bool get vizinhancaSegura {
+    return vizinhos.every((v) => !v._minado);
+  }
+
+  int get qtdeMinasNaVizinhanca {
+    return vizinhos.where((v) => v.minado).length;
+  }
+}
